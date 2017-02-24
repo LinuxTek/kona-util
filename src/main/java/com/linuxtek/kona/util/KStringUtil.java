67beb53f2d8a40319e0d28921f9e360146c331f9
Sharif Alexandre <sharif@fabriq.io>
2017-02-24 10:10:51 -0500

    updates

--- src/main/java/com/linuxtek/kona/util/KStringUtil.java
+++ src/main/java/com/linuxtek/kona/util/KStringUtil.java
@@ -399,6 +399,7 @@ public class KStringUtil {
 	 * \r,\n,\r\n1space <BR>&nbsp; \r,\n,\r\n <BR> \t &nbsp;&nbsp;&nbsp;&nbsp; 2
 	 * spaces 1space
 	 */
+	/*
 	public static String toHtml(String s) {
 		if (s == null)
 			return "";
@@ -422,7 +423,71 @@ public class KStringUtil {
 
 		return html;
 	}
-    
+	*/
+	
+	
+	// http://stackoverflow.com/questions/5134959/convert-plain-text-to-html-text-in-java
+	public static String toHtml(String s) {
+	    if (s == null) {
+	        return null;
+	    }
+
+	    StringBuilder builder = new StringBuilder();
+
+	    boolean previousWasASpace = false;
+
+	    for (char c : s.toCharArray()) {
+	        if (c == ' ') {
+	            if (previousWasASpace) {
+	                builder.append("&nbsp;");
+	                previousWasASpace = false;
+	                continue;
+	            }
+
+	            previousWasASpace = true;
+	        } else {
+	            previousWasASpace = false;
+	        }
+
+	        switch (c) {
+	            case '<':
+	                builder.append("&lt;");
+	                break;
+	            case '>':
+	                builder.append("&gt;");
+	                break;
+	            case '&':
+	                builder.append("&amp;");
+	                break;
+	            case '"':
+	                builder.append("&quot;");
+	                break;
+	            case '\n':
+	                builder.append("<br>");
+	                break;
+	                // We need Tab support here, because we print StackTraces as HTML
+	            case '\t':
+	                builder.append("&nbsp; &nbsp; &nbsp;  &nbsp;");
+	                break;
+	            default:
+	                builder.append(c);
+
+	        }
+	    }
+
+	    String converted = builder.toString();
+
+	    String str = "(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:\'\".,<>?«»“”‘’]))";
+
+	    Pattern patt = Pattern.compile(str);
+
+	    Matcher matcher = patt.matcher(converted);
+
+	    converted = matcher.replaceAll("<a href=\"$1\">$1</a>");
+
+	    return converted;
+	}
+
 
 	@SuppressWarnings("rawtypes")
 	public static String toJson(List list) {
